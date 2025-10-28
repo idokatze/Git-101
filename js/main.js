84245a7258fc936d13787449df4c65bbcc394260
Ido Katzenellenbogen <idokatze@gmail.com>
2025-10-28 16:31:41 +0200

    Apply onclick bigger and center text in ball

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict'
 
-function onBallClick() {
-    console.log('Hi')
+function onBallClick(elBall) {
+    var newSize = parseInt(getComputedStyle(elBall).width) + 50
+    elBall.style.width = newSize + 'px'
+    elBall.style.height = newSize + 'px'
+    elBall.innerText = newSize
 }
