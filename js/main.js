fb784fde7b5e7ff55900b28ae47564a71ad2ffa1
Ido Katzenellenbogen <idokatze@gmail.com>
2025-10-28 17:06:15 +0200

    Limit growth to 400 and reset if needed to 100

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 'use strict'
 
 function onBallClick(elBall) {
-    var newSize = parseInt(getComputedStyle(elBall).width) + 50
-    elBall.style.width = newSize + 'px'
-    elBall.style.height = newSize + 'px'
-    elBall.innerText = newSize
+    var newSize = +elBall.innerText + 50
+
+    if (newSize > 400) {
+        elBall.style.width = 100 + 'px'
+        elBall.style.height = 100 + 'px'
+        elBall.innerText = 100
+    } else {
+        elBall.style.width = newSize + 'px'
+        elBall.style.height = newSize + 'px'
+        elBall.innerText = newSize
+    }
 }
